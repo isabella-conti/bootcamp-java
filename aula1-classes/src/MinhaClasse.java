7fe5cd3fff8b3926ee4f3f83361c7ae3802d2dbd
isabella-conti <ircontif@gmail.com>
2024-04-29 15:04:53 -0300

    declarar variáveis

--- aula1-classes/src/MinhaClasse.java
+++ aula1-classes/src/MinhaClasse.java
@@ -1,9 +1,21 @@
 public class MinhaClasse {
     
-public static void main (String [] args) {
-    System.out.print ("Olá turma, sejam bem-vindos");
-    
-}
+// public static void main (String [] args) {
+//     System.out.print ("Olá turma, sejam bem-vindos");
 
+// }
+public static void main(String[] args) {
+    //tipo + nome bem definido + valor correspondente//
+
+    String meuNome = "Isabella";
+
+    int anoFabricacao = 2003;
+
+    boolean verdadeira = false;
+
+    //manipulação->mudou o valor//
+    anoFabricacao = 2020;
+
+}
 
 }
