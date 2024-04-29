34d754d3964aae1585f41f4e1d1485f7ca916bff
isabella-conti <ircontif@gmail.com>
2024-04-29 15:21:34 -0300

    métodos oo

--- aula1-classes/src/MinhaClasse.java
+++ aula1-classes/src/MinhaClasse.java
@@ -3,21 +3,34 @@ public class MinhaClasse {
 // public static void main (String [] args) {
 //     System.out.print ("Olá turma, sejam bem-vindos");
 
+// // }
+// public static void main(String[] args) {
+//     //tipo + nome bem definido + valor correspondente//
+
+//     String meuNome = "Isabella";
+
+//     int anoFabricacao = 2003;
+
+//     boolean verdadeira = false;
+
+//     //manipulação->mudou o valor//
+//     anoFabricacao = 2020;
+
+//     System.out.print (anoFabricacao);
+
 // }
-public static void main(String[] args) {
-    //tipo + nome bem definido + valor correspondente//
 
-    String meuNome = "Isabella";
+public static void main (String [] args) {
+    String primeiroNome = "Isabella";
+    String segundoNome = "Conti";
 
-    int anoFabricacao = 2003;
+    String nomeCompleto = nomeCompleto (primeiroNome, segundoNome);
+    System.out.println(nomeCompleto);
+}    
 
-    boolean verdadeira = false;
 
-    //manipulação->mudou o valor//
-    anoFabricacao = 2020;
-
-    System.out.print (anoFabricacao);
-
+public static String nomeCompleto (String primeroNome, String segundoNome) {
+    return "Resultado do método " + primeroNome.concat(" ").concat(segundoNome);
 }
 
 }
