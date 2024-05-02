bb57facf9bb9eee88cb31f15352a3ea69609cff7
isabella-conti <ircontif@gmail.com>
2024-05-02 16:36:54 -0300

    tipos primitivos de variaveis

---
+++ estudos/src/edu/isabella/segunda_semana/TiposVariaveis.java
@@ -0,0 +1,21 @@
+package edu.isabella.segunda_semana;
+
+public class TiposVariaveis {
+
+    public static void main(String[] args) throws Exception {
+        //tipos primitivos
+        byte idade = 123;
+        short ano = 2024;
+        int cep = 36037000;
+        long cpf = 15834598702L; //-> encerrar com L
+        float pi = 3.14F; //-> encerrar com F
+        double salarioMinimo = 1325.65;
+        String meuNome = "Isabella Conti";
+
+        final double PI = 3.14; //Constante
+    
+        System.out.println(PI);
+        System.out.print(meuNome);
+    }
+    
+}
