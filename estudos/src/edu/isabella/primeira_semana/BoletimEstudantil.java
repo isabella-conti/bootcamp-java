2774e3e380c4eb61ef74fb778d1b8832204ae4a8
isabella-conti <ircontif@gmail.com>
2024-05-02 16:12:15 -0300

    organizar arquivos

--- estudos/src/edu/isabella/primeira_semana/BoletimEstudantil.java
+++ estudos/src/edu/isabella/primeira_semana/BoletimEstudantil.java
@@ -1,3 +1,4 @@
+package edu.isabella.primeira_semana;
 public class BoletimEstudantil {
 	public static void main(String[] args) {
 		int mediaFinal = 6;
--- estudos/src/edu/isabella/primeira_semana/MinhaClasse.java
+++ estudos/src/edu/isabella/primeira_semana/MinhaClasse.java
@@ -1,3 +1,4 @@
+package edu.isabella.primeira_semana;
 public class MinhaClasse {
     
 // public static void main (String [] args) {
